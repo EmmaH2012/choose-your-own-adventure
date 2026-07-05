eaba297c9b87e6331ff94f90fe6964255a44a2c4
EmmaH2012 <emmakatherinehoward@icloud.com>
2026-07-05 13:00:53 -0400

    Update code.py

--- code.py
+++ code.py
@@ -1,6 +1,7 @@
 begining = input('Welcome! Please type Start to begin!').strip().lower()
 if begining == "start":
     print('You wake up in a mysterious forest you have never seen before,' 
-    'you have no idea' 
-    'how you got there.')
+    ' you have no idea'  
+    ' how you got there.')
+    else: print('please type start')
     input('What do you do? A= Explore B= Cry C= scream for help')
