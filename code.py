6650358cd5f30efa6c791860d2b5d400c3b9d34a
EmmaH2012 <emmakatherinehoward@icloud.com>
2026-07-05 13:12:03 -0400

    Update code.py

--- code.py
+++ code.py
@@ -3,7 +3,8 @@ if begining == "start":
     print('You wake up in a mysterious forest you have never seen before,' 
     ' you have no idea'  
     ' how you got there.')
-   choice one = input('What do you do? A= Explore B= Cry C= scream for help')
+   choice one = input('What do you do?'
+   ' A= Explore B= Cry C= scream for help')
       if choice one == "A":
     print ("You find a note that says 'We have been expecting you, please go to casle.' "
     "It also had a map on how to go to the casle.")
